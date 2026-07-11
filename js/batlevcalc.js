dd3bdfcd1ef34b1130dbe4b357297ba3a4757f34
advaithsunil747-web <advaithsunil747@gmail.com>
2026-07-11 19:52:08 +0530

    Refactor battery calculation and add menu toggle
    Refactor battery calculation and chart update logic. Added hamburger menu functionality.

--- js/batlevcalc.js
+++ js/batlevcalc.js
@@ -387,3 +387,11 @@ function toggleFeedback() {
         box.style.display = "block";
     }
 }
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+});
